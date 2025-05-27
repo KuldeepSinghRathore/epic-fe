173e4d01807ecd8500d6282a2da72693f2678d68
kuldeep singh <kuldeeprathore74@gmail.com>
2025-05-27 10:29:11 +0530

    fixes

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,8 +1,17 @@
 import React from "react"
 import loader from "../../assets/loader.svg"
-export const Loading = () => (
-  <div className="flex flex-col h-screen items-center justify-center ">
-    <span className="text-xl font-bold">Loading...</span>
+
+export const Loading = (props) => (
+  <div className="flex flex-col min-h-screen  items-center justify-center ">
+    <div className=" h-full">
+      <h1 className="font-bold text-xl">
+        Please wait for a minute or Retry after 2 min.
+      </h1>
+      <p className="text-center text-lg font-bold">
+        Server Hosted On Free tier so it takes time
+      </p>
+    </div>
+    {/* <span className="text-xl font-bold">Loading...</span> */}
     <img src={loader} alt="loader" />
   </div>
 )
